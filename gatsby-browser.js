37aa507de7efdbe7d266b3c1a7fbde3c968aef61
Arnau Siches <arnau.siches@digital.cabinet-office.gov.uk>
2018-07-28 11:09:22 +0100

    fix(scroll): Fix scroll manager
    Signed-off-by: Arnau Siches <arnau.siches@digital.cabinet-office.gov.uk>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,13 @@
 exports.onRouteUpdate = () => {
-  const scroller = document.getElementById('scroller');
-  if (scroller != null) scroller.scrollTo(0, 0);
+  const hash = window.location.hash;
+  const targetId = hash.slice(1);
+
+  if (targetId.length > 0) {
+    const target = document.getElementById(target);
+    
+    if (target != null) target.scrollIntoView(true);
+  } else {
+    const scroller = document.getElementById('scroller');
+    if (scroller != null) scroller.scrollTo(0, 0);
+  }
 };
